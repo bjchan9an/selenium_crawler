d10ca0291076dbe3b67eebbaf2271da8fb13dcea
YaFeng Luo <bjchan9an@gmail.com>
2018-12-28 23:31:12 +0800

    test version 0.2

--- test.py
+++ test.py
@@ -8,37 +8,26 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait  
 from selenium.webdriver.support import expected_conditions as EC  
 from selenium.webdriver.common.by import By  
-
+import time
 import pymysql
 
-
-
-
-
-
-def login(browser):
+def login(browser,username,passwd):
     browser.get('https://passport.threatbook.cn/login?service=s&callbackURL=https%3A%2F%2Fs.threatbook.cn%2Fuser')
+    time.sleep(2)
     input_items=browser.find_elements_by_class_name('input')
     input_items[0].send_keys(username)
     input_items[1].send_keys(passwd)
-    check=browser.find_elements_by_class_name('captcha-zone')[0]
-    check.click()
-    login=browser.find_elements_by_xpath("//input[@type='submit']")[0]
-    login.click()
+    check_button=browser.find_elements_by_class_name('captcha-zone')[0]
+    check_button.click()
+    login_button=browser.find_elements_by_xpath("//input[@type='submit']")[0]
+    login_button.click()
     print 'login success...'
-
-    
     # picture check ohohoh >_<    ....remain completed
-
-
-
-    
 
 
 def one_page(browser):
     WebDriverWait(browser,20,0.5).until(
     EC.presence_of_element_located((By.LINK_TEXT, u'下一页')))
-
 
     result=[]
     for tr in browser.find_elements_by_tag_name('tr'):
@@ -77,24 +66,23 @@ def goto_next_page(browser):
 def sql_save():
     db=pymysql.connect('127.0.0.1','root','root','threatbook')
 
-option = webdriver.ChromeOptions()
-#option.add_argument("headless")
-browser= webdriver.Chrome(chrome_options=option)
-url="https://s.threatbook.cn/sample"
-browser.get(url)
-
-pages=1
-while(pages<4):
-    out=one_page(browser)
-    print 'page:',pages,' , itmes num:',len(out)
-    print '********************************************'
-    for i in out:
-        print '======'
-        print i
-    pages+=1
-    goto_next_page(browser)#go to next page
-raw_input('press any key...')
-browser.quit()
-
-        
+def main():
+    option = webdriver.ChromeOptions()
+    #option.add_argument("headless")
+    browser= webdriver.Chrome(chrome_options=option)
+    url="https://s.threatbook.cn/sample"
+    browser.get(url)
+    pages=1
+    while(pages<4):
+        out=one_page(browser)    # add wait method in func one_page() 
+        print 'page:',pages,' , itmes num:',len(out)
+        print '********************************************'
+        for i in out:
+            print '======'
+            print i
+        pages+=1
+        goto_next_page(browser)#go to next page
+    raw_input('press any key...')
+    browser.quit()
+main() 
     
